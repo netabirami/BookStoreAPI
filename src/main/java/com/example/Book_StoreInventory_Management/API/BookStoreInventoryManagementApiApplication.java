517a8256eb42a795f532b06c34f96a6cda37317c
netabirami <net.abirami@gmail.com>
2023-12-01 10:45:10 +0100

    bookstore

---
+++ src/main/java/com/example/Book_StoreInventory_Management/API/BookStoreInventoryManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Book_StoreInventory_Management.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreInventoryManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreInventoryManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Book_StoreInventory_Management/API/BookStoreInventoryManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Book_StoreInventory_Management.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreInventoryManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
